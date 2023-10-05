559f8010601037040f1a522e3c48698e4de008b1
madogap <madogap@gmail.com>
2023-10-05 19:39:05 -0300

    WIPL

--- curso-java-aprendendo/src/org/controlecandidatos/candidatura/ProcessoSeletivo.java
+++ curso-java-aprendendo/src/org/controlecandidatos/candidatura/ProcessoSeletivo.java
@@ -1,5 +1,6 @@
 package org.controlecandidatos.candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
@@ -7,15 +8,46 @@ public class ProcessoSeletivo {
         // analisarCandidato(1000.99);
         // analisarCandidato(2000);
         // analisarCandidato(5000.99);
-        selecaoCandidato();
-        imprimirSelecionados();
+        //selecaoCandidato();
+        //`````imprimirSelecionados();
+        String[]candidatos = { "Toni", "Paulo", "Marcus", "Joao", "Tobi",};
+         for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
 
     }
+
+    
+    static void entrandoEmContato(String candidato){
+        int tetantivasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando) {
+                tetantivasRealizadas++;
+
+            } else {
+                System.out.println("Contato Realizado com sucesso");
+            }
+        } while (continuarTentando && tetantivasRealizadas < 3);
+            if (atendeu) {
+                System.out.println("Conseguimos contato "+ candidato + " NA " + tetantivasRealizadas+ " TENTATIVA");
+            } else {
+                System.out.println("Nao conseguiu contato com o candidato" + candidato + ", NUMERO MAXIMO DE TENTATIVAS " + tetantivasRealizadas);
+            }
+    }
+    
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
+    }
+
     static void imprimirSelecionados(){
         String[] candidatos = { "Toni", "Paulo", "Marcus", "Joao", "Tobi", "Luan", "Kai", "Nal", };
         System.out.println("Imprimindo a lista de candidatos mostrando o indice deles ");
 
-        for (int i =0; i<candidatos.length; i++) {
+        for (int i = 0; i<candidatos.length; i++) {
             System.out.println("O candidato de numero "+ (i+1) + " é " +candidatos[i]);
         }
         System.out.println("\nForma abreviada de escrever o for\n ");
