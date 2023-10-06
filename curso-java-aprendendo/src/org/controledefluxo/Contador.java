6be289735f54f001371fa7e202c8237a4fe9a159
madogap <madogap@gmail.com>
2023-10-06 00:55:24 -0300

    WIP:Controle de Flux

--- curso-java-aprendendo/src/org/controledefluxo/Contador.java
+++ curso-java-aprendendo/src/org/controledefluxo/Contador.java
@@ -5,37 +5,40 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
-
-   
-
         try {
-            //chamar o metodo contendo a logica de contagem
-            System.out.println("Digite o primeiro numero ");
+             System.out.println("Digite o primeiro numero ");
             int parametroUm = terminal.nextInt();
 
             System.out.println("Digite o segundo numero");
             int parametroDois = terminal.nextInt();
 
-            if (parametroDois <= parametroUm) {
-                throw new ParametrosInvaldException("O segundo parâmetro deve ser maior que o primeiro.");
+            if (verificaNumero(parametroUm, parametroDois)) {
+                System.out.println("Os numeros estao na ordem correta");
+            
+            } else {
+                System.out.println("O primeiro numero deve ser menor que o segundo");                   
+
             }
-            contar(int parametroUm, int  parametroDois);
-        } catch (ParametrosInvaldException e) {
-            //Imprimir a mesagem o segundo parametro deve ser maior que o primeiro
-            System.out.println(e.getMessage());
-            }
-        
+        } catch (Exception e) {
+            System.out.println("Leia com atenção. os campos são de números: POR FAVOR DIGITE CORRETAMENTE KKKKKK");
+        }
+       
 
     }
-    
+
     static void contar(int parametroUm, int parametroDois) {
         for (int i = parametroUm; i <= parametroDois; i++) {
             System.out.println(i);
         }
     }
 
-    //static void contar(int parametroUm, int parametroDois) throws ParametrosInvaldException {
-      //  int contagem = parametroDois - parametroUm;
-        // Realizar o for para imprimir os numeroes com base na variravel contagem
-    //}
+    static boolean verificaNumero(int parametroUm, int parametroDois) {
+        return parametroDois > parametroUm;
+    }
+
+    // static void contar(int parametroUm, int parametroDois) throws
+    // ParametrosInvaldException {
+    // int contagem = parametroDois - parametroUm;
+    // Realizar o for para imprimir os numeroes com base na variravel contagem
+    // }
 }
--- curso-java-aprendendo/src/org/controledefluxo/ParametrosInvaldException.java
+++ curso-java-aprendendo/src/org/controledefluxo/ParametrosInvaldException.java
@@ -1,6 +1,5 @@
 package org.controledefluxo;
 
 public class ParametrosInvaldException extends Exception{
-    public ParametrosInvaldException(String message);
-    super(mesagem);
+   
 }
