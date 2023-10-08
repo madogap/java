c6414c75fdd19faefad895718ca612fe0b109446
madogap <madogap@gmail.com>
2023-10-08 09:24:31 -0300

    WIP:Created client class methods

--- lanchonete/src/org/lanchonetes/Cliente.java
+++ lanchonete/src/org/lanchonetes/Cliente.java
@@ -1,5 +1,9 @@
 package org.lanchonetes;
 
 public class Cliente {
-    
+    public void escolherPedido(){System.out.println("ESCOLHENDO O PEDIDO");}
+    public void fazerPedido(){System.out.println("FAZENDO O PEDIDO");}
+    public void pagarConta(){System.out.println("");}
+    public void consultandoSaldoAplicativo(){System.out.println("CONSULTANDO SALDO");}
+    public void pegarPedidoBalcao(){System.out.println("PEGANDO PEDIDO NO BALCAO");}
 }
