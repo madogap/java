bce1a664508154e1f80fcfcd9a47c620f9814802
madogap <madogap@gmail.com>
2023-10-08 09:45:52 -0300

    WIP:class establishment with instances and applying methods

--- lanchonete/src/org/lanchonetes/Estabelecimento.java
+++ lanchonete/src/org/lanchonetes/Estabelecimento.java
@@ -1,5 +1,48 @@
 package org.lanchonetes;
 
 public class Estabelecimento {
-    
+    Cozinheiro cozinheiro = new Cozinheiro();
+    //nao preciasam esta visiveis para todas as aplicacoes
+    cozinheiro.lavarIngredientes();
+    cozinheiro.baterVitaminaLiquidificador();
+    cozinheiro.selecionarIngredientesVitamina();
+    cozinheiro.preparaLanche();
+    cozinheiro.preparaVitamina();
+    cozinheiro.preparaVitamina();
+    //estabelecimento com ciencia
+    cozinheiro.adicionarSucoBalcao();
+    cozinheiro.adicionarLancheBalcao();
+    cozinheiro.adicionarComboBalcao();
+
+    Almoxarife almoxarife = new Almoxarife();
+    //nao preciasam esta visiveis para todas as aplicacoes
+    almoxarife.controlarEntrada();
+    almoxarife.controlarSaida();
+    //acao somente o pacote cozinha precisar saber (default)
+    almoxarife.entregarIngredientes();
+    almoxarife.trocarGas();
+
+    Atendente atendente = new Atendente();
+    atendente.pegarLancheCozinha();
+    atendente.receberPagameto();
+    atendente.servindoMesa();
+    //acoa somente o pacote cozinha precisar saber (default)
+    atendente.trocarGas();
+
+    Cliente cliente = new Cliente();
+    cliente.escolherLanche();
+    cliente.fazerPedido();
+    cliente.pagarConta();
+
+    cliente.pegarPedidoBalcao();
+
+    cliente.consultarSaldoAplicativo();
+
+    cozinheiro.pedirParaTrocarGas(atendente);
+    cozinheiro.pedirParaTrocarGas(almoxarife);
+
+
+
+
+
 }
