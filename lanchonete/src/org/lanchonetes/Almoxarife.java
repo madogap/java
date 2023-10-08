a6f3744ea37bb9d0512f770b411086d526adce43
madogap <madogap@gmail.com>
2023-10-08 09:11:49 -0300

    WIP:Created methods for the Almoxerife class

--- lanchonete/src/org/lanchonetes/Almoxarife.java
+++ lanchonete/src/org/lanchonetes/Almoxarife.java
@@ -1,3 +1,8 @@
 public class Almoxarife {
+    public void controlarEntrada(){System.out.println("CONTROLAR A ENTRADA DO CLINTE EMITIR COMANDA");}
+    public void controlarSaida(){System.out.println("CONTROLANDO SAIDA DO CLIENTE PEGAR COMANDA E VERIFICA SE ESTA PAGA");}
+    public void entregaerIngredientes(){System.out.println("ENTREGANDO INGREDIENTES AO SETOR DE COZINHA");}
+    public void trocarGas(){System.out.println("TROCANDO O GAZ DO SETOR DE COZINHA");}
     
+
 }
--- lanchonete/src/org/lanchonetes/Cozinheiro.java
+++ lanchonete/src/org/lanchonetes/Cozinheiro.java
@@ -1,16 +1,22 @@
 package org.lanchonetes;
 
 public class Cozinheiro {
+
     public void adicionarLancheNoBalcao(){System.out.println("ADICIONADO LANCHE");}
     public void adicionarSucoNoBalcao(){System.out.println("ADICIONAR SUCO NO BALCAO");}
+    public void adicionarComboBalcao(){adicionarLancheNoBalcao();adicionarSucoNoBalcao();}
+
     public void pedirIngredientes(Almoxarife almoxarife){almoxarife.entregarIngredientes();}
     public void pedirParaTrocarGas(Almoxarife meuAmigo){meuAmigo.trocarGas();}
+
     public void fritarIngredientesLanche(){System.out.println("FRITANDO A CARNE E OVO PARA O HAMBURGUER");}
+
     public void selecionarIngredientesVitamina(){System.out.println("SELCIONADO FRUTA, LEITE E SUCO");}
     public void selecionarIngredientesLanche(){System.out.println("SELECIONADO O PAO, SALADA, OVO E CARNE");}
+
     public void prepararCombo(){prepararLanche();preparaVitamina();}
     public void prepararVitamina(){System.out.println("PREPARANDO SUCO");}
     public void prepararLanche(){System.out.println("PREPARANDO LANCHE TIPO HAMBURGUER");}
-    public void adicionarComboBalcao(){adicionarLancheNoBalcao();adicionarSucoNoBalcao();}
+    
 
 }
