a3f3b5154b4ac52288e2665b1ed0538881d3bdfc
madogap <madogap@gmail.com>
2023-10-09 10:56:00 -0300

    WIP:Message design

---
+++ mensagem/mensagens/src/MsnMessenger.java
@@ -0,0 +1,18 @@
+public class MsnMessenger {
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem");
+    }
+
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+
+    public void validarConectadoInternet() {
+        System.out.println("Valindado Status com a internet");
+    }
+
+    public void salvarHistoricoMensagem() {
+        System.out.println("Salvando Historico de Mensagens");
+    }
+
+}
